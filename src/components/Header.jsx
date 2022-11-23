94890d6c0ed9639e623b8fbab9546d7470b53f52
MrAbhishekShrestha <abhishek.shr2000@gmail.com>
2022-11-23 15:56:05 +0800

    header component done | fix page title with favicon

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import "../App.css"
+
+export const Header = () => (
+	<header className="header">
+		<img src="../../world2.svg" alt="world icon" className="logo" />
+		<h1 className="heading">my travel journal</h1>
+	</header>
+);
