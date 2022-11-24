a757bb744eb53858896000ff78695f248d85966f
MrAbhishekShrestha <abhishek.shr2000@gmail.com>
2022-11-24 18:18:57 +0800

    move header logo icon to /assets

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
+import logoUrl from "../assets/world2.svg";
 import "../App.css"
 
 export const Header = () => (
 	<header className="header">
-		<img src="/world2.svg" alt="world icon" className="logo" />
+		<img src={logoUrl} alt="World Icon" className="logo" />
 		<h1 className="heading">my travel journal.</h1>
 	</header>
 );
